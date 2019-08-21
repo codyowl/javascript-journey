90a9cecb2d8acc69d4d6950bb42bdea009fc519f
codyowl <codyowl@gmail.com>
2019-08-21 20:40:21 +0530

    variables added :rocket:

---
+++ variables.js
@@ -0,0 +1,26 @@
+// we can declare variables using three keywords
+// var, let, const
+let myfirstvariable = 5;
+alert(myfirstvariable);
+
+
+/* naming convention for variables:
+   - apart from $ and _ none of the special characters can be used as variables
+   - always use camel case for variable names
+   - digits should not comes first in a variable name
+*/
+
+//valid
+let userName = 'something';
+alert(userName);
+
+
+// using speical characters apart from $ and _
+let user-name = 'something';
+alert(user-name);
+// using digit as a first character for variable
+let 1user = 'something';
+alert(1user);   
+
+// declaring multiple variables
+let first = 1, second = 2, third = 3;
